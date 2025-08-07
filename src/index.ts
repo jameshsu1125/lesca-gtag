3e03e281ee4bc38a8d9d5f20f910dce18253d295
James Hsu <jameshsu1125@gmail.com>
2025-08-07 16:08:51 +0800

    feat: 重構 Gtag 模組，新增安裝、頁面瀏覽和事件追蹤功能

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,64 @@
-export default class YourClass {
-  constructor() {}
+const property = { id: '', debug: false };
+
+declare global {
+  interface Window {
+    dataLayer: Record<string, any>[];
+  }
 }
+
+const install = (gid: string, debug = false) => {
+  const scriptId = 'ga-gtag';
+  property.id = gid;
+  property.debug = debug;
+
+  if (document.getElementById(scriptId)) return;
+
+  const { head } = document;
+  const script = document.createElement('script');
+  script.id = scriptId;
+  script.type = 'text/javascript';
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${gid}`;
+  head.insertBefore(script, head.firstChild);
+
+  window.dataLayer = window.dataLayer || [];
+
+  gtag('js', new Date());
+  gtag('config', gid);
+  if (debug) console.log(`%c gid = ${gid}`, 'color: #bada55');
+};
+
+const insert = (gid: string) => {
+  property.id = gid;
+};
+
+const gtag = function (...args: any[]) {
+  window.dataLayer.push(args);
+};
+
+const pv = function (page_title: string, parameters: Record<string, any> = {}) {
+  const { id, debug } = property;
+  if (!id) console.warn('[lesca-gtag]', 'gid not found.');
+  gtag('event', 'page_view', { page_title, ...parameters });
+  if (debug) console.log(`%c pv = ${page_title}`, 'color: #bada55');
+};
+
+const event = function (
+  page_title: string,
+  event_name: string = '',
+  parameters: Record<string, any> = {},
+) {
+  const { id, debug } = property;
+  if (!id) console.error('[lesca-gtag]', 'gid not found.');
+
+  gtag('event', `${page_title}-${event_name}`, {
+    page_title,
+    event_name,
+    ...parameters,
+  });
+
+  if (debug) console.log(`%c event = ${page_title}-${event_name}`, 'color: #bada55');
+};
+
+const Gtag = { install, pv, event, insert, gtag };
+export default Gtag;
