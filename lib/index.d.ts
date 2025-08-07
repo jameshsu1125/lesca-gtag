6a63512e42887cc66c659236180c3864844b63cd
James Hsu <jameshsu1125@gmail.com>
2025-08-07 16:22:16 +0800

    feat: 更新 package.json，新增 Gtag 模組，並調整 tsconfig 和相關檔案結構

---
+++ lib/index.d.ts
@@ -0,0 +1,13 @@
+declare global {
+    interface Window {
+        dataLayer: Record<string, any>[];
+    }
+}
+declare const Gtag: {
+    install: (gid: string, debug?: boolean) => void;
+    pv: (page_title: string, parameters?: Record<string, any>) => void;
+    event: (page_title: string, event_name?: string, parameters?: Record<string, any>) => void;
+    insert: (gid: string) => void;
+    gtag: (...args: any[]) => void;
+};
+export default Gtag;
